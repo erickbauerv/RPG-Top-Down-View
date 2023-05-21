49a0629606023b54ead0eec5de8a37c95f0c5c8b
erickbauerv <hullbauer@gmail.com>
2023-05-21 14:47:15 -0300

    Implementada animação para o movimento do player.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
     private Rigidbody2D playerRigidbody2D;
-    private Vector2 newPosition;
+    private Vector2 movement;
+    private Animator playerAnimator;
     private float horizontalInput;
     private float verticalInput;
 
@@ -14,6 +15,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRigidbody2D = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,11 +24,15 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
-        newPosition = new Vector2(horizontalInput, verticalInput).normalized * speed;
+        movement = new Vector2(horizontalInput, verticalInput).normalized * speed;
+
+        playerAnimator.SetFloat("Horizontal", horizontalInput);
+        playerAnimator.SetFloat("Vertical", verticalInput);
+        playerAnimator.SetFloat("Movement", movement.sqrMagnitude);
     }
 
     void FixedUpdate()
     {
-        playerRigidbody2D.MovePosition(playerRigidbody2D.position + (newPosition * Time.fixedDeltaTime));
+        playerRigidbody2D.MovePosition(playerRigidbody2D.position + (movement * Time.fixedDeltaTime));
     }
 }
